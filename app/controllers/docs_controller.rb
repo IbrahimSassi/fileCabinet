0a8535558fd51bf9600f8f5f55d27d735a6a445f
ibrahimsassi <ibrahim.sassi@esprit.tn>
2016-09-02 14:23:41 +0000

    Association between Users and Docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -11,11 +11,15 @@ class DocsController < ApplicationController
   end
 
   def new
-    @doc = Doc.new
+    #before adding users
+    #@doc = Doc.new
+
+    #after adding user
+    @doc= current_user.docs.build
   end
 
   def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
 
     if @doc.save
       redirect_to @doc
